f88ce88de27f54e6928ac882b41844fee388b860
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2021-09-20 16:33:27 -0400

    Installing packages for selenium

---
+++ ATUnivTestProject/ATUnivTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATUnivTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AgileThought")]
+[assembly: AssemblyProduct("ATUnivTestProject")]
+[assembly: AssemblyCopyright("Copyright © AgileThought 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0fcf8b9-a66a-407c-b75c-a3d526f8ff1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
